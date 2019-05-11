bb14a20481cf9a86b959d7149630c66a2af9d6ab
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2019-05-11 18:23:18 +0200

    clean up byte queue, add implementation utilizing BufferManager

---
+++ dotnet/lib/AbstractByteQueue.cs
@@ -0,0 +1,104 @@
+﻿/////////////////////////////////////////////////////////////////////
+//  wrapper around byte array to simplify operations with it
+/////////////////////////////////////////////////////////////////////
+
+using System;
+using System.IO;
+
+namespace dant02.snippets.dotnet.lib
+{
+    public abstract class AbstractByteQueue
+    {
+        protected int allocatedSize = 4096;
+        protected byte[] buffer = null;
+        protected int readIndex = 0;
+        protected int writeIndex = 0;
+
+        public int Count { get { return writeIndex - readIndex; } }
+
+        public void Add(byte data)
+        {
+            Resize(1);
+
+            buffer[writeIndex] = data;
+            writeIndex += 1;
+        }
+
+        public void Add(byte[] data, int count)
+        {
+            this.Add(data, count, 0);
+        }
+
+        public void Add(byte[] data, int count, int offset)
+        {
+            Resize(count);
+
+            Buffer.BlockCopy(data, offset, buffer, writeIndex, count);
+            writeIndex += count;
+        }
+
+        public void Clear()
+        {
+            this.readIndex = this.writeIndex = 0;
+        }
+
+        public int Get(byte[] data, int offset, int count)
+        {
+            int result = count;
+            if (this.Count < result)
+                result = this.Count;
+
+            Buffer.BlockCopy(this.buffer, readIndex, data, offset, count);
+            readIndex += result;
+
+            if (readIndex == writeIndex)
+                readIndex = writeIndex = 0;
+
+            return result;
+        }
+
+        public Int32 GetInt32()
+        {
+            if (writeIndex - readIndex < 4)
+                throw new InvalidOperationException("Not enough data in buffer");
+
+            Int32 result = BitConverter.ToInt32(this.buffer, readIndex);
+            readIndex += 4;
+
+            if (readIndex == writeIndex)
+                readIndex = writeIndex = 0;
+
+            return result;
+        }
+
+        public Int64 GetInt64()
+        {
+            if (writeIndex - readIndex < 8)
+                throw new InvalidOperationException("Not enough data in buffer");
+
+            Int64 result = BitConverter.ToInt64(this.buffer, readIndex);
+            readIndex += 8;
+
+            if (readIndex == writeIndex)
+                readIndex = writeIndex = 0;
+
+            return result;
+        }
+
+        public int ReadFromStream(Stream stream)
+        {
+            var length = allocatedSize - writeIndex;
+            var read = stream.Read(buffer, writeIndex, length);
+            writeIndex += read;
+            return read;
+        }
+
+        public void WriteToStream(Stream stream)
+        {
+            var length = writeIndex - readIndex;
+            stream.Write(buffer, readIndex, length);
+        }
+
+        protected abstract void Resize(int length);
+    }
+}
---
+++ dotnet/lib/ManagedByteQueue.cs
@@ -0,0 +1,42 @@
+﻿/////////////////////////////////////////////////////////////////////
+//  implementation using utilizing BufferManager
+/////////////////////////////////////////////////////////////////////
+
+#if !NETSTANDARD1_6 && !NETSTANDARD2_0
+
+using System;
+using System.ServiceModel.Channels;
+
+namespace dant02.snippets.dotnet.lib
+{
+    public class ManagedByteQueue : AbstractByteQueue, IDisposable
+    {
+        private readonly BufferManager bufferManager = null;
+
+        public ManagedByteQueue(BufferManager manager)
+        {
+            this.bufferManager = manager;
+            this.buffer = manager.TakeBuffer(allocatedSize);
+        }
+
+        public ManagedByteQueue(BufferManager manager, int size)
+        {
+            this.bufferManager = manager;
+            this.allocatedSize = size;
+            this.buffer = manager.TakeBuffer(allocatedSize);
+        }
+
+        public void Dispose()
+        {
+            this.bufferManager.ReturnBuffer(buffer);
+        }
+
+        protected override void Resize(int length)
+        {
+            if (allocatedSize - writeIndex < length)
+                throw new NotImplementedException("Cannot resize managed byte queue");
+        }
+    }
+}
+
+#endif
