ee141b31b38c03f76febcb442f2b34e375806947
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2023-01-08 23:47:47 +0100

    Update project type Remove ManagedByteQueue

--- dotnet/lib/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("446ff898-2960-4374-be85-1e497832ef8f")]
